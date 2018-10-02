ee8d8e607e7bb341fc22cf7bc6b0f359db5629fb
Ricky <rickybroers.rb@gmail.com>
2018-10-02 15:53:26 +0200

    Get first user story working

---
+++ src/main/java/nl/han/dea/ricky/MainResource.java
@@ -0,0 +1,15 @@
+package nl.han.dea.ricky;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/")
+public class MainResource {
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String helloWorld() {
+        return "Hello World!";
+    }
+}
