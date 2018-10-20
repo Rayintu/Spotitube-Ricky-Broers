f51ae2177da4fd7d246c3c1dbbc4fcb01f484aa6
Ricky <rickybroers.rb@gmail.com>
2018-10-20 19:27:19 +0200

    further work in database implementation

--- src/main/java/nl/han/dea/ricky/persistence/AccountDAO.java
+++ src/main/java/nl/han/dea/ricky/persistence/AccountDAO.java
@@ -9,8 +9,12 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.UUID;
 
 public class AccountDAO {
 
@@ -86,23 +90,73 @@ public class AccountDAO {
     }
 
     public UserToken getToken(LoginCredentials creds) {
+        Date date = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+
+        String currentDate = sdf.format(date);
         try (
                 Connection connection = connectionFactory.getConnection();
-                PreparedStatement statement = connection.prepareStatement(
-                        "SELECT accounts.full_name, t.token FROM accounts JOIN tokens t on accounts.user = t.user WHERE accounts.user = ?");
+                PreparedStatement statement = connection.prepareStatement("SELECT accounts.full_name,t.user, t.token, t.valid_until FROM accounts JOIN tokens t on accounts.user = t.user WHERE accounts.user = ?");
+                PreparedStatement newUserStatement = connection.prepareStatement("SELECT * FROM accounts WHERE user = ?")
         ) {
             statement.setString(1, creds.getUser());
             ResultSet resultSet = statement.executeQuery();
 
             if (resultSet.next()) {
-                return new UserToken(resultSet.getString("token"), resultSet.getString("full_name"));
+                String fullName = resultSet.getString("full_name");
+                if (sdf.parse(currentDate).before(resultSet.getDate("valid_until")) && resultSet.getString("token") != null) {
+                    return new UserToken(resultSet.getString("token"), fullName);
+                } else {
+                    return new UserToken(createNewTokenForUserAndUpdateItInDatabase(resultSet.getString("user")), fullName);
+                }
             } else {
-                throw new SQLException();
+                newUserStatement.setString(1, creds.getUser());
+                ResultSet newUserResultset = newUserStatement.executeQuery();
+                if (newUserResultset.next()) {
+                    return new UserToken(createNewTokenForNewUserAndInsertItIntoDatabase(newUserResultset.getString("user")), newUserResultset.getString("full_name"));
+                } else {
+                    throw new SQLException("Something went wrong");
+                }
             }
 
 
         } catch (SQLException e) {
             throw new RuntimeException(e);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
         }
+    }
+
+    private String createNewTokenForNewUserAndInsertItIntoDatabase(String user) {
+        UUID uuid = UUID.randomUUID();
+
+        try (
+                Connection connection = connectionFactory.getConnection();
+                PreparedStatement statement = connection.prepareStatement("INSERT INTO tokens (user, token, valid_until) VALUES (?,?,DATE_ADD(curdate(), INTERVAL 1 DAY))");
+        ) {
+            statement.setString(1, user);
+            statement.setString(2, uuid.toString());
+            statement.execute();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return uuid.toString();
+    }
+
+    private String createNewTokenForUserAndUpdateItInDatabase(String username) {
+        UUID uuid = UUID.randomUUID();
+
+        try (
+                Connection connection = connectionFactory.getConnection();
+                PreparedStatement statement = connection.prepareStatement("UPDATE tokens SET token= ?, valid_until = DATE_ADD(curdate(), INTERVAL 1 DAY) WHERE user = ?");
+
+        ) {
+            statement.setString(1, uuid.toString());
+            statement.setString(2, username);
+            statement.execute();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return uuid.toString();
     }
 }
---
+++ src/main/java/nl/han/dea/ricky/persistence/PlaylistDAO.java
@@ -0,0 +1,9 @@
+package nl.han.dea.ricky.persistence;
+
+public class PlaylistDAO {
+    private ConnectionFactory connectionFactory;
+
+    public PlaylistDAO() {
+        connectionFactory = new ConnectionFactory();
+    }
+}
